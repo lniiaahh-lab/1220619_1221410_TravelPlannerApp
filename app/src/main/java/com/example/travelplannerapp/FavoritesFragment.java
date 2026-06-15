6bf07609673f9685b1ac39b622e7f339f4b2e42e
ZainabAbuawwad <1221410@student.birzeit.edu>
2026-06-15 23:30:17 +0300

    Add placeholder fragments and fix login SharedPreferences

---
+++ app/src/main/java/com/example/travelplannerapp/FavoritesFragment.java
@@ -0,0 +1,19 @@
+package com.example.travelplannerapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.fragment.app.Fragment;
+
+public class FavoritesFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        TextView tv = new TextView(getContext());
+        tv.setText("Favorites - Coming Soon");
+        tv.setTextSize(24);
+        tv.setPadding(40, 40, 40, 40);
+        return tv;
+    }
+}
--- app/src/main/java/com/example/travelplannerapp/HomeActivity.java
+++ app/src/main/java/com/example/travelplannerapp/HomeActivity.java
@@ -116,12 +116,5 @@ public class HomeActivity extends AppCompatActivity
         finish();
     }
 
-    @Override
-    public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
+
 }
