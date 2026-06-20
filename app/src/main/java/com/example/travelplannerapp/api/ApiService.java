456d1c82ad492b887026e4adf0081d9b2e960664
ZainabAbuawwad <1221410@student.birzeit.edu>
2026-06-20 19:16:09 +0300

    Fix the figures of the trips

--- app/src/main/java/com/example/travelplannerapp/api/ApiService.java
+++ app/src/main/java/com/example/travelplannerapp/api/ApiService.java
@@ -8,6 +8,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface ApiService {
-    @GET("v3/qs/6a353eedda38895dfedd8957")
+    @GET("v3/qs/6a36bc35da38895dfee35c42")
     Call<TripResponse> getTrips();
 }
