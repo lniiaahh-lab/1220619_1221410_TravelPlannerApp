8ccb5c2e3c42bfbbef72a7d40b72a4197edb399e
lniiaahh-lab <Iniiaahh@gmail.com>
2026-06-20 10:40:34 +0300

    AdminReservationsFragment class Added

---
+++ app/src/main/java/com/example/travelplannerapp/AdminReservationsFragment.java
@@ -0,0 +1,22 @@
+package com.example.travelplannerapp;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class AdminReservationsFragment extends Fragment {
+
+    @Override
+    public android.view.View onCreateView(
+            android.view.LayoutInflater inflater,
+            android.view.ViewGroup container,
+            Bundle savedInstanceState) {
+
+        TextView tv = new TextView(getContext());
+
+        tv.setText("All Reservations");
+
+        return tv;
+    }
+}
